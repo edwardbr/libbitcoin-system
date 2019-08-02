c67885c68a906f10fa8684eebd50546d73b7ce4a
Edward Boggis-Rolfe <edward@boggis-rolfe.com>
2019-08-02 15:05:14 +0100

    patched secp256k1 to be compatable with test suite, abc

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,4 +2,4 @@ hunter_config(Boost VERSION 1.70.0-p0)
 
 hunter_config(secp256k1
     URL https://github.com/edwardbr/secp256k1/archive/master.tar.gz
-    SHA1 2435bc8418d63f811d5de9a45738e98eac6409d8)
+    SHA1 7229336a56a3d0171cc6c6055bf4f0635435e0cb)

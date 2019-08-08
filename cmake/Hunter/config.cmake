74179212e97fb192a54bf020fb601d716227ed9d
Edward Boggis-Rolfe <edward@boggis-rolfe.com>
2019-08-08 15:18:06 +0100

    added more support for icu

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,4 +2,16 @@ hunter_config(Boost VERSION 1.70.0-p0)
 
 hunter_config(secp256k1
     URL https://github.com/edwardbr/secp256k1/archive/master.tar.gz
-    SHA1 7229336a56a3d0171cc6c6055bf4f0635435e0cb)
+    SHA1 7229336a56a3d0171cc6c6055bf4f0635435e0cb
+    CMAKE_ARGS 
+        CONFIG_MACRO_U_USING_ICU_NAMESPACE=1
+        CONFIG_MACRO_U_DISABLE_RENAMING=0)
+
+
+hunter_config(
+    ICU
+    VERSION 55.1-p4
+    CMAKE_ARGS 
+        U_USING_ICU_NAMESPACE=1
+        U_DISABLE_RENAMING=0
+)

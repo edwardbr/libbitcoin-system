24bcea0b9ef7f0cb7678f3b63fabd13a4eb323e1
Edward Boggis-Rolfe <eboggis-rolfe@trustportal.org>
2019-10-16 17:12:03 +0100

    fixed for win32

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,11 +1,16 @@
-hunter_config(Boost VERSION 1.70.0-p0)
+hunter_config(Boost VERSION 1.70.0-p0 
+	CMAKE_ARGS
+		BUILD_SHARED_LIBS=ON)
 
 hunter_config(secp256k1
     URL https://github.com/edwardbr/secp256k1/archive/master.tar.gz
-    SHA1 7229336a56a3d0171cc6c6055bf4f0635435e0cb
+    SHA1 c630cef1a69e1b8e79a121e7a3e2ba7c636bdd03
     CMAKE_ARGS 
         CONFIG_MACRO_U_USING_ICU_NAMESPACE=1
-        CONFIG_MACRO_U_DISABLE_RENAMING=0)
+        CONFIG_MACRO_U_DISABLE_RENAMING=0
+		SECP256K1_BUILD_TEST=off
+		SECP256K1_ENABLE_MODULE_RECOVERY=on
+		SECP256K1_ENABLE_MODULE_SCHNORR=off)
 
 
 hunter_config(
